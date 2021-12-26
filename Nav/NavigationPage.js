48394b8a8e60e2d9a99c69c5777d757f2b52cfa8
Amr R. Mohamed <mero@Amrs-MacBook-Air.local>
2021-12-26 03:00:15 -0330

    Created the home icion and will work on the other icions

--- Nav/NavigationPage.js
+++ Nav/NavigationPage.js
@@ -1,21 +1,20 @@
 import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 export default function Nav() {
+
     const NavIcon = <View style={styles.NavIconStyle1}>
-      <LinearGradient colors={['#4c669f', '#3b5998', '#192f6a']} style={styles.NavIconStyle}>
-      <Text style={styles.button}>
-        Sign in with Facebook
-      </Text>
-    </LinearGradient>
+        <LinearGradient 
+          start={{ x: 0.35, y: 0 }} and end={{ x: 0, y: 0.95 }}
+          locations={[0,0.25,0.95]} 
+          colors={['#4651D5','#6548DE','#FF007A']} 
+          style={styles.NavIconStyle}
+        >
+          <Icon name="home" size={50} color="#94B3FD" />
+      </LinearGradient>
     </View>
-    // <LinearGradient
-    //     // Button Linear Gradient
-    //     colors={['#4c669f', '#3b5998', '#192f6a']}
-    //     style={styles.linearGradient}>
-    //     <Text style={styles.buttonText}>Sign in with Facebook</Text>
-    //     </LinearGradient>
 
     return (NavIcon);
   }
@@ -24,36 +23,19 @@ export default function Nav() {
 // Later on in your styles..
 const styles = StyleSheet.create({
     NavIconStyle1: {
-      // backgroundColor: 'transparent',
-      // fontSize: 15,
-      // color: '#fff',
-      // width: '30%',
       bottom: 1,
       margin: "2.5%",
       marginBottom: "12.5%",
       position: 'absolute',
       bottom:0,
-      right:0,
-      // width: 100,
-      // height: 100,
-      // borderRadius: 100 / 2,
-      // backgroundColor: "red",
+      right:0, 
     },
     NavIconStyle: {
-      // backgroundColor: 'transparent',
-      // fontSize: 15,
-      // color: '#fff',
-      // width: '30%',
-      // bottom: 1,
-      // margin: "7.5%",
-      // marginBottom: "15%",
-      // position: 'absolute',
-      // bottom:0,
-      // right:0,
       width: 80,
       height: 80,
-      borderRadius: 80 / 2,
-      // backgroundColor: "red",
+      borderRadius: 80 / 2, 
+      alignItems: 'center',
+    justifyContent: 'center',
     },
     button: {
       padding: 15,
