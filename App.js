9021877444dc822a8f6abc107de0657118d4a6ff
Amr R. Mohamed <mero@Amrs-MacBook-Air.local>
2021-12-27 21:27:12 -0330

    Different orgnization and creating different files

--- App.js
+++ App.js
@@ -1,52 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Nav from './Nav/NavigationPage';
 import { LinearGradient } from 'expo-linear-gradient';
+import IconNav from './Navigation/NavigationPage';
 
-export default function App() {
+export default class App extends Component {
 
-  const NavBar = <View style={styles.NavBarStyle}>
-      <Text>Lets see</Text>
+  NavBar = () => <View style={styles.NavBarStyle}>
+      <Text style={{marginTop: "10%", marginLeft: "45%", color: "white"}}>Lets see</Text>
     </View> 
   
-  const NotiPage = <LinearGradient 
-    start={{ x: 0.95, y: 0 }} and end={{ x: 0, y: 0.95 }}
-    locations={[0,0.25,0.65]} 
-    colors={['#39A2DB', '#F38BA0','#502064']}
-    style={styles.NotiPageStyle}
-  >
-      <Nav 
-        style={styles.NavIconStyle}
-      /> 
-  </LinearGradient>
+  MainContaint = () => <LinearGradient style={styles.MainContaintStyle}
+      start={{ x: 0.95, y: 0 }} and end={{ x: 0, y: 0.95 }}
+      locations={[0,0.25,0.65]} colors={['#39A2DB', '#F38BA0','#502064']}
+    >
+      <IconNav/>
+    </LinearGradient>
   
-  const ContainerArea = <View style={styles.ContainerAreaStyle}>
-      {NavBar}
-      {NotiPage}
+  
+  ContainerArea = () => <View style={styles.container}>
+      {this.NavBar()}
+      {this.MainContaint()}
     </View>
 
-  return (ContainerArea);
+
+  render() {
+    return (this.ContainerArea());
+  }
 }
 
 const styles = StyleSheet.create({
-  ContainerAreaStyle: {
+  container: {
+    flex: 1,
+    alignItems: "center",
     display: 'flex',
     flexDirection: 'column',
     width: "100%",
     height: "100%",
-    // backgroundColor: 'orange', //to check the location and how much it covers
+    backgroundColor: 'orange', //to check the location and how much it covers
   },
   NavBarStyle: {
     width: "100%",
     height: "8%",
     backgroundColor: "blue",
   },
-  NotiPageStyle: {
+  MainContaintStyle: {
     width: "100%",
     height: "100%",
     flex: 1,
     // alignItems: 'center',
     // justifyContent: 'center',
-    // backgroundColor: "red",
   },
 });
