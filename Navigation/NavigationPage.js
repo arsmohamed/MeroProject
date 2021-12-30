f0df9681a70d49d50d36dd3cfb0845465d2215a9
Amr R. Mohamed <mero@Amrs-MacBook-Air.local>
2021-12-30 17:54:54 -0330

    looking for way to do tranformation to the icons

--- Navigation/NavigationPage.js
+++ Navigation/NavigationPage.js
@@ -11,28 +11,33 @@ export default function Nav (props) {
   /********************************************  Icon Chnage Function *********************************************************/
   
   /********************************************  Animation Function *********************************************************/
-  const [ChangeIcon, setChangeIcon] = useState(false); // to change the icon 
+
   const animation = useRef(new Animated.Value(0)).current;
-  console.log(animation)
-  const [open, setOpen] = useState(0)
+  const mode = useRef(new Animated.Value(0)).current;
+   
+  const [ChangeIcon, setChangeIcon] = useState(false); // to change the icon 
+  const [Open1, setOpen] = useState(0); // to change open value 
+  
   const toggleMenu = () => {
+    // const toValue = open1 ? 0 : 1
 
-      setChangeIcon(true)
+    Animated.spring( animation, {
+      toValue: 1,
+      friction: 5,
+      useNativeDriver: true 
+    }).start(); 
+    setChangeIcon(true)
+    setOpen(1)  
   }
-  /********************************************  Notification Icons *********************************************************/
-  const AddIcon = <LinearGradient 
-      start={{ x: 0.35, y: 0 }} and end={{ x: 0, y: 0.95 }}
-      locations={[0,0.25,0.95]} 
-      colors={['#4651D5','#6548DE','#FF007A']} 
-      style={styles.NavIconStyle} 
-    >
-      <TouchableWithoutFeedback onPress={()=>setChangeIcon(false)}>
-        <Animated.View >
-          <Icon name="plus" size={50} color="#94B3FD" />
-        </Animated.View>
-      </TouchableWithoutFeedback> 
-    </LinearGradient> 
+  const thermometerX = mode.interpolate({
+    inputRange: [0, 1],
+    outputRange: [-24, -100]
+});
 
+const thermometerY = mode.interpolate({
+    inputRange: [0, 1],
+    outputRange: [-50, -100]
+});
   const Home = <LinearGradient 
       start={{ x: 0.35, y: 0 }} and end={{ x: 0, y: 0.95 }}
       locations={[0,0.25,0.95]} 
@@ -46,11 +51,45 @@ export default function Nav (props) {
       </TouchableWithoutFeedback> 
     </LinearGradient> 
 
+  // const DiaryStyle = {
+  //   transform: [
+  //   {
+  //       scale: animation
+  //     },
+  //     {
+  //         inputRange: [0, 1],
+  //         translateY: animation.interpolate({
+  //             outputRange: [0, -70]
+  //           })
+  //         }
+  //       ]
+  //     }
+            
+            
+  const chooseIcon = () => {
+    setChangeIcon(false)
+    setOpen(0) 
+  }
+  const AddIcon = <LinearGradient 
+      start={{ x: 0.35, y: 0 }} and end={{ x: 0, y: 0.95 }}
+      locations={[0,0.25,0.95]} 
+      colors={['#4651D5','#6548DE','#FF007A']} 
+      style={styles.NavIconStyle} 
+    >
+      <TouchableWithoutFeedback onPress={chooseIcon}>
+        <Animated.View >
+          <Icon name="plus" size={50} color="#94B3FD" />
+        </Animated.View>
+      </TouchableWithoutFeedback> 
+    </LinearGradient> 
+
+  /********************************************  Notification Icons *********************************************************/
+
   const Diary = <LinearGradient 
       start={{ x: 0.35, y: 0 }} and end={{ x: 0, y: 0.95 }}
       locations={[0,0.25,0.95]} 
       colors={['#1C84E4','#1C84DB','#21D7FF']} 
-      style={[styles.NavIconStyle, styles.secondary]}
+      style={[styles.NavIconStyle, styles.secondary, {left: thermometerX, top: thermometerY}]}
     >
       <TouchableWithoutFeedback>
         <Animated.View >
@@ -99,7 +138,9 @@ export default function Nav (props) {
     </LinearGradient> 
 
     const NavIcon = <View style={[styles.IconsPageAre, props.style]}>
-      {Meal}{Note}{Task}{Diary}{ChangeIcon ? AddIcon : Home}
+      {Open1? [Meal, Note, Task, Diary] : console.log("is it working ")}
+      
+      {ChangeIcon ? AddIcon : Home}
     </View>
 
     return (NavIcon);
